f526853df5d43e3dd085523d1abba81720950d6e
Samphathai vonemany <samphathainv@gmail.com>
2024-01-08 19:21:25 +0700

    chang font all with id

--- src/component/context.jsx
+++ src/component/context.jsx
@@ -5,16 +5,15 @@ import Logo from '../assets/profile.jpeg'
 function context() {
   return (
     <div className="w3-content w3-container w3-padding-64" id="about">
-      <h3 className="w3-center">ABOUT ME</h3>
+      <h3 className="w3-center" id='head'>ກ່ຽວກັບ ຂ້ອຍ</h3>
       <p className="w3-center"><em>I Like Programeing</em></p>
-      <p>ສະບາຍດີທຸກຄົນ ຂ້ອຍຊື່ວ່າທ້າວ ສາມພະໄທ ວອນມະນີ ເກີດວັນທີ 08-10-2000 ທີ່ສ້າງເວ໋ບຂື້ນມານີ້ເປັນການເເນະນຳໂຕຂ້ອຍເອງ ແລະ ຄວາມຝັນຂອງຂ້ອຍ. <br /> ບ້ານເກີດຂອງຂ້ອຍແມ່ນຢູ່ທີ່ ນະຄອນໄກສອນ ພົມວິຫານ ເເຂວງສະຫວັນນະເຂດ ບ້ານນາເລົ່າ ຕອນປະຖົມສືກສາຢູ່ທີ່ ໂຮງຮຽນສະຫວ່າງຫຼັກ 3 ຫຼັງຈາກຈົບໄດ້ຮຽນ ຕໍ່ທີ່ໂຮງຮຽນມັດທະຍົມຕອນຕົ້ນ ເລກ 3 ຫລັງຈາກຈົບມໍ 4 ໄດ້ໄປຮຽນຕໍ່ທີ່ ໂຮງຮຽນ ມສ ສະຫວັນ (ເລກ4) ແລະ ສືກສາປະລິນຍາຕິທີ່ມະຫາວິທະຍາໄລແຫ້ງຊາດ ຄະນະວິທະຍາສາດ ສາຂາ internet Technology ຈົບການສືກສາໃນປິ 2022.<br /></p>
+      <p>ສະບາຍດີທຸກຄົນ ຂ້ອຍຊື່ວ່າທ້າວ ສາມພະໄທ ວອນມະນີ(ສາມ) ເກີດວັນທີ 08-10-2000 ທີ່ສ້າງເວ໋ບຂື້ນມານີ້ເປັນການເເນະນຳໂຕຂ້ອຍເອງ ແລະ ຄວາມຝັນຂອງຂ້ອຍ. <br /> ບ້ານເກີດຂອງຂ້ອຍແມ່ນຢູ່ທີ່ ນະຄອນໄກສອນ ພົມວິຫານ ເເຂວງສະຫວັນນະເຂດ ບ້ານນາເລົ່າ ຕອນປະຖົມສືກສາຢູ່ທີ່ ໂຮງຮຽນສະຫວ່າງຫຼັກ 3 ຫຼັງຈາກຈົບໄດ້ຮຽນ ຕໍ່ທີ່ໂຮງຮຽນມັດທະຍົມຕອນຕົ້ນ ເລກ 3 ຫລັງຈາກຈົບມໍ 4 ໄດ້ໄປຮຽນຕໍ່ທີ່ ໂຮງຮຽນ ມສ ສະຫວັນ (ເລກ4) ແລະ ສືກສາປະລິນຍາຕິທີ່ມະຫາວິທະຍາໄລແຫ້ງຊາດ ຄະນະວິທະຍາສາດ ສາຂາ internet Technology ຈົບການສືກສາໃນປິ 2022.<br /></p>
       <div className="w3-row">
         <div className="w3-col m6 w3-center w3-padding-large">
           <p><b><i className="fa fa-user w3-margin-right"></i>My Name is Samphathai VONEMANY</b></p><br />
           <img src={Logo} className="w3-round w3-image w3-opacity w3-hover-opacity-off" alt="Photo of Me" width="500" height="50" />
         </div>
-
-        <div className="w3-col m6 w3-hide-small w3-padding-large">
+        <div className="w3-col m6 w3-center w3-padding-large">{/*if w3-hide-small oject while hid */}
           <p>Welcome to my website. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
             dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
